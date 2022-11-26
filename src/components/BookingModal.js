59f59a8cf150b24f8a0d3aee16207a5bbe5a8f00
takdirhossain <takdirhossain35@gmail.com>
2022-11-27 04:26:12 +0600

    set modal

--- src/components/BookingModal.js
+++ src/components/BookingModal.js
@@ -12,12 +12,11 @@ const BookingModal = ({bookings}) => {
               <h3 className="text-lg font-bold">Want To Book This?</h3>
               <form  className='grid grid-cols-1 gap-3 mt-10'>
                   <input type="text" value={user?.email} readOnly className="input w-full input-bordered " />
-                  <select name="slot" className="select select-bordered w-full">
-                      
-                  </select>
-                  <input name="name"  type="text" placeholder="Your Name" className="input w-full input-bordered" />
-                  <input name="email"  readOnly type="email" placeholder="Email Address" className="input w-full input-bordered" />
-                  <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
+                 
+                  <input name="name" value={bookings.productName} readOnly type="text" placeholder="Your Name" className="input w-full input-bordered" />
+                  <input name="price" value={bookings.sellingprice} readOnly type="email" placeholder="Email Address" className="input w-full input-bordered" />
+                  <input name="phone" type="text" placeholder="Your Number" className="input w-full input-bordered" />
+                  <input name="location" type="text" placeholder="Your Location" className="input w-full input-bordered" />
                   <br />
                   <input className='btn btn-accent w-full' type="submit" value="Submit" />
               </form>
